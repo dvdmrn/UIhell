480d1c9d05c99f2573350d229effeeba4c18c061
dvdmrn <dgm.marino@gmail.com>
2018-10-11 00:42:18 -0700

    first commit

---
+++ UI_hell/Assets/UIReactions.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIReactions : MonoBehaviour {
+
+	Vector3 scale;
+
+	// Use this for initialization
+	void Start () {
+	scale = transform.localScale; 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void Rescale(Slider slider){
+		float val = slider.value;
+		transform.localScale = new Vector3(scale.x+val,scale.y+val,scale.z+val);
+		print("rescaling with: "+scale);
+	}
+
+	public void Clicky(){
+		transform.Rotate(new Vector3(Random.Range(0,360),Random.Range(0,360),Random.Range(0,360)));
+	}
+}
